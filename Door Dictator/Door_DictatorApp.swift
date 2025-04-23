73584d000003ea9b8bb10de527a5caeec00b4492
Jia Chen <github@jiachen.app>
2025-04-23 20:55:44 +0800

    Initial Commit

---
+++ Door Dictator/Door_DictatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Door_DictatorApp.swift
+//  Door Dictator
+//
+//  Created by Jia Chen Yee on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Door_DictatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
